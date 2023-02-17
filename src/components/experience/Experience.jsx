9386ae1f3968585b522ed6c2ed1fd67f639c4bcb
emivnajera <emivelasquez2000@hotmail.com>
2023-02-16 23:50:19 -0600

    services component css.

--- src/components/experience/Experience.jsx
+++ src/components/experience/Experience.jsx
@@ -1,9 +1,224 @@
 import React from 'react'
 import './experience.css'
+import {RiHtml5Fill} from 'react-icons/ri'
+import {SiCss3} from 'react-icons/si'
+import {SiJavascript} from 'react-icons/si'
+import {SiReact} from 'react-icons/si'
+import {SiJava} from 'react-icons/si'
+import {SiAngular} from 'react-icons/si'
+import {SiPython} from 'react-icons/si'
+import {SiDjango} from 'react-icons/si'
+import {SiGoland} from 'react-icons/si'
+import {SiMicrosoft} from 'react-icons/si'
+import {FaNodeJs} from 'react-icons/fa'
+import {SiFlask} from 'react-icons/si'
+import {SiMysql} from 'react-icons/si'
+import {SiPostgresql} from 'react-icons/si'
+import {SiMicrosoftsqlserver} from 'react-icons/si'
+import {SiOracle} from 'react-icons/si'
+import {SiAmazondynamodb} from 'react-icons/si'
+import {SiMongodb} from 'react-icons/si'
+import {SiAmazonaws} from 'react-icons/si'
+import {SiGooglecloud} from 'react-icons/si'
+import {SiDocker} from 'react-icons/si'
+import {SiKubernetes} from 'react-icons/si'
+import {SiGithub} from 'react-icons/si'
+import {SiGitkraken} from 'react-icons/si'
+import {SiLinux} from 'react-icons/si'
+import {BsTerminalFill} from 'react-icons/bs'
 
 const Experience = () => {
   return (
-    <section id='experience'>Experience</section>
+    <section id='experience'>
+      <h5>What Skills I Have</h5>
+      <h2>My Experience</h2>
+
+      <div className="container experience_container">
+        <div className="experience_frontend">
+          <h3>Frontend Developtment</h3>
+          <div className="experience_content">
+            <article className='experience_details'>
+              <RiHtml5Fill/>
+              <h4>HTML</h4>
+              <small className='text-light'>Experienced</small>
+            </article>
+
+            <article className='experience_details'>
+              <SiCss3/>
+              <h4>CSS</h4>
+              <small className='text-light'>Experienced</small>
+            </article>
+
+            <article className='experience_details'>
+              <SiJavascript/>
+              <h4>Javascript</h4>
+              <small className='text-light'>Experienced</small>
+            </article>
+
+            <article className='experience_details'>
+              <SiReact/>
+              <h4>ReactJS</h4>
+              <small className='text-light'>Experienced</small>
+            </article>
+
+            <article className='experience_details'>
+              <SiAngular/>
+              <h4>AngularJS</h4>
+              <small className='text-light'>Experienced</small>
+            </article>
+          </div>
+        </div>
+
+        <div className="experience_backend">
+          <h3>Backend Developtment</h3>
+          <div className="experience_content">
+            <article className='experience_details'>
+              <SiJava/>
+              <h4>Java</h4>
+              <small className='text-light'>Experienced</small>
+            </article>
+
+            <article className='experience_details'>
+              <SiPython/>
+              <h4>Python</h4>
+              <small className='text-light'>Experienced</small>
+            </article>
+
+            <article className='experience_details'>
+              <SiDjango/>
+              <h4>Django</h4>
+              <small className='text-light'>Experienced</small>
+            </article>
+
+            <article className='experience_details'>
+              <SiGoland/>
+              <h4>Golang</h4>
+              <small className='text-light'>Experienced</small>
+            </article>
+
+            <article className='experience_details'>
+              <SiMicrosoft/>
+              <h4>C#</h4>
+              <small className='text-light'>Experienced</small>
+            </article>
+
+            <article className='experience_details'>
+              <SiMicrosoft/>
+              <h4>.NET</h4>
+              <small className='text-light'>Experienced</small>
+            </article>
+
+            <article className='experience_details'>
+              <SiJavascript/>
+              <h4>Javascript</h4>
+              <small className='text-light'>Experienced</small>
+            </article>
+
+            <article className='experience_details'>
+              <FaNodeJs/>
+              <h4>NodeJS</h4>
+              <small className='text-light'>Experienced</small>
+            </article>
+
+            <article className='experience_details'>
+              <SiFlask/>
+              <h4>Flask</h4>
+              <small className='text-light'>Experienced</small>
+            </article>
+
+            <article className='experience_details'>
+              <SiMysql/>
+              <h4>MySQL</h4>
+              <small className='text-light'>Experienced</small>
+            </article>
+
+            <article className='experience_details'>
+              <SiPostgresql/>
+              <h4>PostgreSQL</h4>
+              <small className='text-light'>Experienced</small>
+            </article>
+
+            <article className='experience_details'>
+              <SiMicrosoftsqlserver/>
+              <h4>SQL Server</h4>
+              <small className='text-light'>Experienced</small>
+            </article>
+
+            <article className='experience_details'>
+              <SiOracle/>
+              <h4>Oracle</h4>
+              <small className='text-light'>Experienced</small>
+            </article>
+
+
+            <article className='experience_details'>
+              <SiAmazondynamodb/>
+              <h4>DynamoDB</h4>
+              <small className='text-light'>Experienced</small>
+            </article>
+
+            <article className='experience_details'>
+              <SiMongodb/>
+              <h4>MongoDB</h4>
+              <small className='text-light'>Experienced</small>
+            </article>
+          </div>
+        </div>
+
+        <div className="experience_others">
+          <h3>Others</h3>
+          <div className="experience_content">
+            <article className='experience_details'>
+              <SiAmazonaws/>
+              <h4>Amazon Web Services</h4>
+              <small className='text-light'>Experienced</small>
+            </article>
+
+            <article className='experience_details'>
+              <SiGooglecloud/>
+              <h4>Google Cloud Platform</h4>
+              <small className='text-light'>Experienced</small>
+            </article>
+
+            <article className='experience_details'>
+              <SiDocker/>
+              <h4>Docker</h4>
+              <small className='text-light'>Experienced</small>
+            </article>
+
+            <article className='experience_details'>
+              <SiKubernetes/>
+              <h4>Kubernetes</h4>
+              <small className='text-light'>Experienced</small>
+            </article>
+
+            <article className='experience_details'>
+              <SiGithub/>
+              <h4>GitHub</h4>
+              <small className='text-light'>Experienced</small>
+            </article>
+
+            <article className='experience_details'>
+              <SiGitkraken/>
+              <h4>GitKraken</h4>
+              <small className='text-light'>Experienced</small>
+            </article>
+
+            <article className='experience_details'>
+              <SiLinux/>
+              <h4>Linux</h4>
+              <small className='text-light'>Experienced</small>
+            </article>
+
+            <article className='experience_details'>
+              <BsTerminalFill/>
+              <h4>Terminal Prompt</h4>
+              <small className='text-light'>Experienced</small>
+            </article>
+          </div>
+        </div>
+      </div>
+    </section>
   )
 }
 
--- src/components/services/Services.jsx
+++ src/components/services/Services.jsx
@@ -1,8 +1,114 @@
 import React from 'react'
+import {BiCheck} from 'react-icons/bi'
+import './services.css'
 
 const Services = () => {
   return (
-    <section id='services'>Services</section>
+    <section id='services'>
+      <h5>What I Offer</h5>
+      <h2>Services</h2>
+
+      <div className="container services_container">
+        <article className='service'>
+          <div className="service_head">
+            <h3>Frontend Developer</h3>
+          </div>
+
+          <ul className='service_list'>
+            <li>
+              <BiCheck className='service_list icon'/>
+              <p>Contenido</p>
+            </li>
+
+            <li>
+              <BiCheck className='service_list icon'/>
+              <p>Contenido</p>
+            </li>
+
+            <li>
+              <BiCheck className='service_list icon'/>
+              <p>Contenido</p>
+            </li>
+
+            <li>
+              <BiCheck className='service_list icon'/>
+              <p>Contenido</p>
+            </li>
+
+            <li>
+              <BiCheck className='service_list icon'/>
+              <p>Contenido</p>
+            </li>
+          </ul>
+        </article>
+
+        <article className='service'>
+          <div className="service_head">
+            <h3>Backend Developer</h3>
+          </div>
+
+          <ul className='service_list'>
+            <li>
+              <BiCheck className='service_list icon'/>
+              <p>Contenido</p>
+            </li>
+
+            <li>
+              <BiCheck className='service_list icon'/>
+              <p>Contenido</p>
+            </li>
+
+            <li>
+              <BiCheck className='service_list icon'/>
+              <p>Contenido</p>
+            </li>
+
+            <li>
+              <BiCheck className='service_list icon'/>
+              <p>Contenido</p>
+            </li>
+
+            <li>
+              <BiCheck className='service_list icon'/>
+              <p>Contenido</p>
+            </li>
+          </ul>
+        </article>
+
+        <article className='service'>
+          <div className="service_head">
+            <h3>Database Administrator</h3>
+          </div>
+
+          <ul className='service_list'>
+            <li>
+              <BiCheck className='service_list icon'/>
+              <p>Contenido</p>
+            </li>
+
+            <li>
+              <BiCheck className='service_list icon'/>
+              <p>Contenido</p>
+            </li>
+
+            <li>
+              <BiCheck className='service_list icon'/>
+              <p>Contenido</p>
+            </li>
+
+            <li>
+              <BiCheck className='service_list icon'/>
+              <p>Contenido</p>
+            </li>
+
+            <li>
+              <BiCheck className='service_list icon'/>
+              <p>Contenido</p>
+            </li>
+          </ul>
+        </article>
+      </div>
+    </section>
   )
 }
 
